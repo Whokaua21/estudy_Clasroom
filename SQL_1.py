aea42305b9d725a149449a8d3ce255f10b228d2d
Kaua_Eduardo_KL <139010512+Whokaua21@users.noreply.github.com>
2023-12-09 15:55:25 -0300

    Add files via upload

---
+++ SQL_1.py
@@ -0,0 +1,37 @@
+# Today let's go learning a little from SQL
+# Remenber what i'm from Brazil so some moments i can write in portugues 
+# And my english not is good becase i'm still improving
+
+# SQL:SQl is a Bank form Data he serve to store informat and data to you use later
+# Bankdata:Bankdata they are places to store things 
+# I go speak some Bankdata,SQl,MySQL,SGBD is etc
+# SQL is some very important to a programmer that  easy mutch to find a informat or data
+# SQL is a Linguage
+
+# Today don't have very things so i go put my code
+
+# use `u274908554_108A`;
+# drop table Aluno_84;
+# create table Aluno_84(
+# id_alus int(10),
+# nome varchar(40),
+# idade_aluno int(12)
+# );
+
+# drop table Cursos_84;
+# create table Cursos_84(
+# Id_curs int,
+# nome_curso varchar(40),
+# carga_horaria float
+# );
+
+# create table Matricula_84(
+# id_matricula int(6),
+# id_aluno int (65),
+# data_matricula varchar(40)
+# );
+
+# insert into Cursos_84(Id_curs,nome_curso,carga_horaria) values (12,'Full Stak',22.2);
+# select * from Cursos_84
+
+# put that in mySQL if you know use that help in row 34 and 35 :)
